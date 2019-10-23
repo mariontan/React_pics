dff5d3e35ad3ea92655a3df5aaf4380df792bbbe
Ivan <marionltan@gmail.com>
2019-10-23 09:57:19 +0800

    removed the warnings by adding keys

---
+++ src/components/ImageList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ImageList=(props)=>{
+
+	const images =props.images.map(({description, id, urls})=>{
+		// key is assingednot the root elelment of the return satatement
+		return <img alt={description} key={id} src={urls.regular}/>
+	});
+	return(
+		<div>{images}</div>
+	);
+}
+
+export default ImageList;
