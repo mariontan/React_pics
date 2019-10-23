ae62dddc52e33c913315213c3023e92110cff5fe
Ivan <marionltan@gmail.com>
2019-10-23 14:21:50 +0800

    made the display nicer

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import './ImageList.css';
+import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList=(props)=>{
 
-	const images =props.images.map(({description, id, urls})=>{
+	const images =props.images.map((image)=>{
 		// key is assingednot the root elelment of the return satatement
-		return <img alt={description} key={id} src={urls.regular}/>
+		return <ImageCard key={image.id} image={image}/>
 	});
 	return(
-		<div>{images}
-		</div>
+		<div className="image-list">{images}</div>
 	);
 }
 
